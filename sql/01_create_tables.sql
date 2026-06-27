e7a24b6d91530d8ae385c71d744b6f602516c32f
MichaelSantiagoPhysics <michael.jimenez1@hp.com>
2026-06-27 15:13:22 +0000

    1.0.3

--- sql/01_create_tables.sql
+++ sql/01_create_tables.sql
@@ -1,8 +1,67 @@
-WITH Customers as (
+-- WITH Customers as (
 
-    SELECT *, 
+--     SELECT *, 
+--         ROW_NUMBER() OVER (PARTITION BY customer_ID, customer_name ORDER BY customer_name) AS num_duplicates
+
+--     FROM read_csv(
+--         '/workspaces/Nuaav/client_b/Customer.CSV',
+--         header = true,
+--         auto_detect = true,
+--         strict_mode = false,
+--         null_padding = true,
+--         skip = 1
+--     )
+--     WHERE customer_id NOT LIKE '%END OF FILE%' 
+--         AND customer_name IS NOT NULL
+--         AND email IS NOT NULL
+--         AND segment IS NOT NULL
+
+--         AND TRIM(COALESCE(customer_id, '')) <> ''
+--         AND TRIM(COALESCE(customer_name, '')) <> ''
+--         AND TRIM(COALESCE(email, '')) <> '' 
+--         AND TRIM(COALESCE(segment, '')) <> ''
+        
+--     ORDER BY customer_name
+-- )
+-- SELECT * EXCLUDE (num_duplicates) FROM Customers 
+-- WHERE num_duplicates = 1
+--     AND email NOT LIKE '%@@%'
+-- ;
+
+
+-- -- Hay dos tablas de Customers
+
+-- -- WITH Customers_a as (
+-- --     select * from read_csv('/workspaces/Nuaav/client_a/Customer.csv')
+-- -- )
+-- -- ;
+
+
+-- WITH  Customers_a AS    (
+--         SELECT * REPLACE(REPLACE(REPLACE(email, '@@', '@'), '..', '.') AS email ), 
+
+--             ROW_NUMBER() OVER (PARTITION BY customer_id, email ) as num_duplicates
+        
+--          FROM  read_csv(
+--             '/workspaces/Nuaav/client_a/Customer.csv',
+--             skip = 1, 
+--             header = true, 
+--             null_padding = true
+--             )
+--         WHERE first_name IS  NOT NULL AND
+--             last_name IS NOT NULL AND 
+--             email IS NOT NULL
+--     )
+-- SELECT * EXCLUDE (num_duplicates) FROM Customers_a
+-- WHERE num_duplicates = 1
+-- ;
+
+
+
+WITH Customers_a as (
+
+    SELECT * REPLACE(REPLACE(REPLACE(email, '@@', '@'), '..', '.') AS email, REPLACE(is_active, '<-- invalid email', '') AS is_Active), 
         ROW_NUMBER() OVER (PARTITION BY customer_ID, customer_name ORDER BY customer_name) AS num_duplicates
-
 
     FROM read_csv(
         '/workspaces/Nuaav/client_b/Customer.CSV',
@@ -18,27 +77,12 @@ WITH Customers as (
         AND segment IS NOT NULL
 
         AND TRIM(COALESCE(customer_id, '')) <> ''
-        AND TRIM(COALESCE(customer_name, '')) <> ''
-        AND TRIM(COALESCE(email, '')) <> '' 
-        AND TRIM(COALESCE(segment, '')) <> ''
         
     ORDER BY customer_name
-)
-SELECT * EXCLUDE (num_duplicates) FROM Customers 
-WHERE num_duplicates = 1
-    AND email NOT LIKE '%@@%'
+),
 
-;
-
-
--- WITH Customers_a as (
---     select * from read_csv('/workspaces/Nuaav/client_a/Customer.csv')
--- )
--- ;
-
-
-WITH  Customers_a AS    (
-        SELECT * REPLACE(REPLACE(REPLACE(email, '@@', '@'), '..', '.') AS email ), 
+Customers_b AS (
+        SELECT * REPLACE(REPLACE(REPLACE(email, '@@', '@'), '..', '.') AS email, REPLACE(is_active, '<-- invalid email', '') AS is_Active), 
 
             ROW_NUMBER() OVER (PARTITION BY customer_id, email ) as num_duplicates
         
@@ -50,10 +94,25 @@ WITH  Customers_a AS    (
             )
         WHERE first_name IS  NOT NULL AND
             last_name IS NOT NULL AND 
-            email IS NOT NULL
-    )
-SELECT * EXCLUDE (num_duplicates) FROM Customers_a
-WHERE num_duplicates = 1
+            email IS NOT NULL AND
+            TRIM(COALESCE(customer_id, '')) <> ''
+)
+
+SELECT cust_a.* EXCLUDE(num_duplicates), cust_b.* EXCLUDE(num_duplicates, email, first_name, last_name)
+    FROM Customers_a cust_a
+        LEFT JOIN Customers_b cust_b 
+            ON UPPER(cust_b.email) = UPPER(cust_a.email)
+
+
+
+    WHERE cust_a.num_duplicates = 1
+    -- AND cust_b.num_duplicates = 1
+
 ;
 
+-- SELECT * FROM READ_CSV('/workspaces/Nuaav/client_b/Customer.CSV');
 
+-- SELECT * FROM READ_CSV('/workspaces/Nuaav/client_a/Customer.csv');
+
+-- CUST-A-0005,Chris,Evans,cevans@example,PLATINUM,Web,true
+-- C-CUST-5001,Chris Evans,cevans@example.com,VIP,true
